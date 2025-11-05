799754faf1e20f8eafa52d3793455ec6cab0280f
ivannprocyk-ui <ivan.nprocyk@gmail.com>
2025-11-05 14:13:26 -0300

    Configure Vite with tsconfig paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     allowedHosts: true,
   },
