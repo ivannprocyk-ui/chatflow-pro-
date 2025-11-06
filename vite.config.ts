1048f13ba247eaf13ad2fe2ec284c32b2a6bc2e6
ivannprocyk-ui <ivan.nprocyk@gmail.com>
2025-11-06 00:33:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    allowedHosts: true,
-  },
-  build: {
-    chunkSizeWarningLimit: 5000,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
-});
+{
+  "exclude": [
+    "server/**",
+    "workers/**",
+    "functions/**",
+    "api/**",
+    "index.ts"
+  ]
+}
